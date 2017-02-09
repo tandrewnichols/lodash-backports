de9d95b3f349cb3df1e10a34f14720dbb1964a2e
Andrew Nichols <tandrewnichols@gmail.com>
2017-02-09 07:45:20 -0500

    Comment out code climate token

--- gulp/codeclimate.js
+++ gulp/codeclimate.js
@@ -5,7 +5,7 @@ gulp.task('codeclimate', function() {
   if (process.version.indexOf('v4') > -1) {
     gulp.src('coverage/lcov.info', { read: false })
       .pipe(codeclimate({
-        token: '98c10ee52b1795a3b1d48df9ad4ad6a046a45af5ac9010bc146de7f56bcc503b'
+        //token: '98c10ee52b1795a3b1d48df9ad4ad6a046a45af5ac9010bc146de7f56bcc503b'
       }));
   }
 });
